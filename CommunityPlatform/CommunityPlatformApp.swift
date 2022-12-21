fd0283ddd73858a14d4ffb90853d7d1e9c688c3c
Batınay Ünsel <batinayunsel@gmail.com>
2022-12-22 01:31:25 +0300

    Initial commit

---
+++ CommunityPlatform/CommunityPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityPlatformApp.swift
+//  CommunityPlatform
+//
+//  Created by Batınay Ünsel on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CommunityPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommunityPlatform/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CommunityPlatform
+//
+//  Created by Batınay Ünsel on 22.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SignIn()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
